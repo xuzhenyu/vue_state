3c65b74ee2d37fd01a9bc4298e922ddfc11cff1c
xuzhenyu <xiiaoyu@vip.qq.com>
2018-06-05 17:54:25 +0800

    修改提交

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -25,6 +25,7 @@ const actions ={
     addAction(context){
         context.commit('add',10);
         setTimeout(()=>{context.commit('reduce')},3000);
+        console.log('我比reduce提前执行');
     },
     reduceAction({commit}){
         commit('reduce')
